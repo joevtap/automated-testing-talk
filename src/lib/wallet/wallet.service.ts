df4666b707cd7c6c1db4803aed8702280273133b
joevtap <joelvtorres2024@gmail.com>
2025-10-25 17:48:56 -0300

    refactor: use dependency injection in application service

--- src/lib/wallet/wallet.service.ts
+++ src/lib/wallet/wallet.service.ts
@@ -1,15 +1,16 @@
-import { sql } from "../db/db";
+import { sql as defaultSql } from "../db/db";
 import { Wallet, Id } from "./wallet";
+import type postgres from 'postgres';
 
 export class WalletService {
-  constructor() { }
+  constructor(private sql: postgres.Sql = defaultSql) { }
 
   public async transferFunds(
     senderId: Id,
     recipientId: Id,
     amount: number
   ): Promise<[Wallet, Wallet]> {
-    const [newSender, newRecipient] = await sql.begin(async (sql) => {
+    const [newSender, newRecipient] = await this.sql.begin(async (sql) => {
       const [senderRow] = await sql`
         SELECT owner_id, balance
         FROM wallets
@@ -27,8 +28,8 @@ export class WalletService {
       if (!senderRow) throw new Error('Sender wallet not found')
       if (!recipientRow) throw new Error('Recipient wallet not found')
 
-      const sender = new Wallet(senderRow.owner_id, senderRow.balance)
-      const recipient = new Wallet(recipientRow.owner_id, recipientRow.balance)
+      const sender = new Wallet(senderRow.owner_id, Number(senderRow.balance))
+      const recipient = new Wallet(recipientRow.owner_id, Number(recipientRow.balance))
 
       const [newSender, newRecipient] = sender.transfer(amount, recipient)
 
